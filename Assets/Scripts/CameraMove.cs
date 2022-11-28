513eb89d2844da75d9bb31607b03033f494b29da
Eyerward <matthieubruffa@gmail.com>
2022-11-28 11:59:05 +0100

    Déplacement de caméra
    vérifier potentiel plugin

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] LayerMask CameraSliderLayerMask;
+
+    float prevX = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 0)
+        {
+            prevX = 0;
+        }
+        else
+        {
+
+
+            if (prevX != 0)
+            {
+                float deltaX = prevX - Camera.main.ScreenToWorldPoint(Input.mousePosition).x; ;
+
+                Camera.main.transform.Translate(deltaX, 0, 0);
+
+
+            }
+
+
+
+            prevX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+
+
+
+            // float posX = (hit.point.x) /** -1*/;
+            //transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+
+        }
+    }
+
+}
