5cfc696d3a4641aa25a20896d85e8ca0b89261f8
Eyerward <matthieubruffa@gmail.com>
2022-12-07 12:07:42 +0100

    Functionnal Wheel Rotation

---
+++ Assets/Scripts/GameObjects/PadLock.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadLock : MonoBehaviour
+{
+    [SerializeField] string combination;
+    [SerializeField] GameObject wheel1;
+    [SerializeField] GameObject wheel2;
+    [SerializeField] GameObject wheel3;
+    [SerializeField] GameObject wheel4;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GameObjects/Wheel.cs
+++ Assets/Scripts/GameObjects/Wheel.cs
@@ -1,12 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core.Easing;
 
 public class Wheel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void OnMouseUpAsButton()
+    public float numValue;
+    public float rotate= 0;
+
+    private void Start()
     {
-        
+        numValue = 0;
+        numValue.ToString();
+    }
+    private void OnMouseDown()
+    {
+        rotate -= 36f;
+        //if (rotate <= -360) rotate = 0;
+        DOTween.Kill(gameObject);
+        Quaternion quat = Quaternion.Euler(rotate, 0, 0);
+        transform.DOLocalRotateQuaternion(quat, 0.5f).SetEase(Ease.OutBounce);
+        Debug.Log(rotate);
+    }
+
+    void ChangeValue()
+    {
+        numValue++;
+    }
+
+    private void Update()
+    {
+
     }
 }
