15f16e108cfc46ac02b15179b04541d60cecb8c9
Eyerward <matthieubruffa@gmail.com>
2023-01-06 18:21:35 +0100

    ça commence à ramer

--- Assets/Scripts/GameObjects/Scratcher.cs
+++ Assets/Scripts/GameObjects/Scratcher.cs
@@ -7,21 +7,25 @@ using UnityEngine.UIElements;
 
 public class Scratcher : MonoBehaviour
 {
+    [SerializeField] GameObject scratchFX;
     int scratchCount = 0;
 
     private void OnMouseDown()
     {
         if (scratchCount >= 4)
         {
-            
+            DOTween.Kill(gameObject);
+            GameObject Burst = Instantiate(scratchFX);
+            Burst.transform.position = transform.position;
+            Destroy(gameObject);
         }
         else
         {
             scratchCount++;
-            transform.eulerAngles = new Vector3(0, 0, scratchCount*4);
+            transform.eulerAngles = new Vector3(0, 0, scratchCount*8);
             DOTween.Kill(gameObject);
             Quaternion quat = Quaternion.Euler(0, 0, 0);
-            transform.DOLocalRotateQuaternion(quat, 0.3f).SetEase(Ease.OutElastic);
+            transform.DOLocalRotateQuaternion(quat, 0.5f).SetEase(Ease.OutElastic);
         }
     }
 }
